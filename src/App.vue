0ee127eb74eeca44da223a8fd312f55ba4b4f53f
fahri <fahri-can@gmx.at>
2022-07-12 18:55:32 +0200

    Add file.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg" style="background-color: #56BEE4;">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <img src="../src/assets/logo.png" alt="" width="30"
-             height="24"
+      <router-link class="navbar-brand" to="/">
+        <img src="../src/assets/logo_getthingdone.png" alt="get_things_done logo" width="344"
+             height="100"
              class="d-inline-block align-text-top">
-        Bootstrap
-      </a>
+      </router-link>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
@@ -22,9 +21,26 @@
       </div>
     </div>
   </nav>
-
   <router-view/>
 </template>
 
 <style scoped>
+
+nav a {
+  font-weight: bold;
+  color: white;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 4px;
+  margin: 10px;
+}
+
+nav a.router-link-exact-active {
+  color: white;
+  background: black;
+}
+
 </style>
+<script>
+export default {}
+</script>
