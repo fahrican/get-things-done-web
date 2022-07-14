7ab49694d1789670a3c2131b28c4689e1891ebeb
fahri <fahri-can@gmx.at>
2022-07-14 18:13:18 +0200

    Start fetching data from endpoint with Axios.

--- src/views/OpenTasksView.vue
+++ src/views/OpenTasksView.vue
@@ -1,21 +1,67 @@
 <template>
   <h1>All Open Tasks</h1>
+  <p>{{ quote }}</p>
+<!--  <button @click="createTask">create task</button>
+  <button @click="createPost">create post</button>-->
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import {defineComponent, ref} from "vue";
+import TaskApi from "@/services/TaskApi";
+
 
 export default defineComponent({
   name: "OpenTasksView",
-  async created() {
-    const result = await this.fetchTasks();
-    console.log('tasks: ' + result)
-  },
-  methods: {
-    async fetchTasks() {
-      const res = await fetch('http://localhost:8080/api/open-tasks');
-      return res.json();
+  setup() {
+
+    const quote = ref('');
+
+    const loadQuote = async () => {
+      try {
+        const response = await TaskApi.getTasks();
+        quote.value = response.data[0].description
+        console.log(response.data);
+      } catch (err) {
+        console.log('error loadQuote: ' + err)
+      }
     }
+
+    loadQuote()
+
+/*    const createTask = () => {
+      axios.post('http://localhost:8080/api/create',
+          JSON.stringify({
+            description: "Create WEB FE",
+            is_reminder_set: false,
+            is_task_open: true,
+            created_on: "2022-06-22T23:35:53",
+            time_interval: "24h",
+            time_taken: 0
+          })).then(response => {
+        console.log(response);
+      })
+    }
+
+    const createPost = () => {
+      axios.post('https://jsonplaceholder.typicode.com/posts',
+          JSON.stringify({
+            userId: 1,
+            title: 'test',
+            description: 'test desc'
+          })).then(response => {
+        console.log(response);
+      })
+    }*/
+
+    /*
+
+        axios.get('http://localhost:8080/api/open-tasks').then(response => {
+          quote.value = response.data[0]
+          console.log(quote.value);
+        });
+    */
+
+    return {quote}
   }
 });
 </script>
