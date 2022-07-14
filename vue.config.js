9a440f52852ccbb91195a16d056b441474907737
fahri <fahri-can@gmx.at>
2022-07-14 18:11:18 +0200

    Remove proxy from config file.

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    proxy: 'https://justluxurylifestyle.com/'
-  }
+    transpileDependencies: true,
+    devServer: {}
 })
