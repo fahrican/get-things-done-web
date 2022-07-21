7f288c28ddff62557b8b6fde2bd19ce695c8904d
fahri <fahri-can@gmx.at>
2022-07-21 23:52:20 +0200

    Display all data from task.

--- src/views/SingleTaskView.vue
+++ src/views/SingleTaskView.vue
@@ -4,6 +4,11 @@
   <p>createdOn: {{ saveTask.createdOn }}</p>
   <p>time interval: {{ saveTask.timeInterval }}</p>
   <p>priority: {{ saveTask.priority }}</p>
+  <p>reminder set: {{ saveTask.isReminderSet }}</p>
+  <p>started on: {{ saveTask.startedOn }}</p>
+  <p>finished on: {{ saveTask.finishedOn }}</p>
+  <p>time taken: {{ saveTask.timeTaken }}</p>
+  <p>task open: {{ saveTask.isTaskOpen }}</p>
 </template>
 
 <script lang="ts">
