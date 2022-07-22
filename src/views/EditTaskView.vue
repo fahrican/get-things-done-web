e82a4d62e8b57c9a1e450df60020f7a2cc4c1f4d
fahri <fahri-can@gmx.at>
2022-07-22 20:29:30 +0200

    Create form field for editing task.

--- src/views/EditTaskView.vue
+++ src/views/EditTaskView.vue
@@ -1,15 +1,20 @@
 <template>
+  <h1>Edit Task</h1>
   <form @submit="onSubmit" class="add-form">
     <div class="form-control">
-      <label>Task</label>
-      <input type="text" v-model="myTask.description" name="text" placeholder="Add Task"/>
+      <label>Task description</label>
+      <input type="text" v-model="myTask.description" name="description" placeholder="Add Task"/>
+    </div>
+    <div class="form-control">
+      <label>Created on</label>
+      <input type="text" v-model="myTask.createdOn" name="created_on" placeholder="2022-06-22T23:35:53"/>
     </div>
     <div class="form-control">
       <label>Time Interval</label>
       <input type="text" v-model="myTask.timeInterval" name="time_interval" placeholder="24h"/>
     </div>
     <div class="form-control">
-      <label>Time Taken</label>
+      <label>Time Taken in hours</label>
       <input type="number" v-model="myTask.timeTaken" name="time_taken" placeholder="0"/>
     </div>
     <div>
@@ -24,9 +29,23 @@
       <input type="radio" v-model="myTask.priority" name="priority" value="HIGH">
       <label for="high">HIGH</label><br>
     </div>
+    <div class="form-control">
+      <label>Started on</label>
+      <input type="text" v-model="myTask.startedOn" name="started_on" placeholder="2022-06-22T23:35:53"/>
+    </div>
+    <div class="form-control">
+      <label>Finished on</label>
+      <input type="text" v-model="myTask.finishedOn" name="finished_on" placeholder="2022-06-22T23:35:53"/>
+    </div>
+
     <div class="form-control form-control-check">
-      <label>Set Reminder</label>
-      <input type="checkbox" v-model="myTask.isReminderSet" name="reminder"/>
+      <label>Set Reminder:</label>
+      <input type="checkbox" v-model="myTask.isReminderSet" name="is_reminder_set"/>
+    </div>
+
+    <div class="form-control form-control-check">
+      <label>Task open:</label>
+      <input type="checkbox" v-model="myTask.isTaskOpen" name="is_task_open"/>
     </div>
 
     <input type="submit" value="Save Task" class="btn btn-block"/>
