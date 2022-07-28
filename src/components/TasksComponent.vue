eddff4d84fd2812e047cd0ceeb94d64c68dccf6a
fahri <fahri-can@gmx.at>
2022-07-29 01:51:41 +0200

    Adapt logic.

--- src/components/TasksComponent.vue
+++ src/components/TasksComponent.vue
@@ -6,72 +6,52 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import TaskComponent from './TaskComponent.vue';
-import {defineComponent, PropType} from "vue";
+import {PropType, defineEmits, reactive, defineProps} from "vue";
 import TaskApi from "@/services/TaskApi";
 import {TaskState} from "@/types/TaskState";
+import {TaskRequest} from "@/types/TaskRequest";
 
 
-export default defineComponent({
-  name: "TasksComponent",
-  emits: ["delete-task"],
-  components: {
-    TaskComponent
-  },
-  props: {
-    taskState: {
-      type: Object as PropType<TaskState>,
-      required: true
-    }
-  },
-  data() {
-    return {
-      tasks: Array<{
-        id: number
-        description: string
-        isReminderSet: boolean
-        createdOn: string
-        startedOn: string
-        finishedOn: string
-        priority: number
-        timeInterval: string
-        timeTaken: number
-        isTaskOpen: boolean
-      }>()
-    }
-  },
-  async created() {
-    this.tasks = await this.fetchTasks();
-  },
-  methods: {
-    async deleteTask(id: number): Promise<void> {
-      if (confirm('Are you sure, you want to delete?')) {
-        const res = await TaskApi.deleteTask(id);
+defineEmits(["delete-task"]);
 
-        res.status === 200
-            ? (this.tasks = this.tasks.filter((task) => task.id !== id))
-            : alert('Error while deleting task');
-      }
-    },
-    async fetchTasks() {
-      try {
-        this.tasks = [];
-        let response = null;
-        if (this.taskState === TaskState.OPEN) {
-          response = await TaskApi.getOpenTasks();
-        } else if (this.taskState === TaskState.CLOSED) {
-          response = await TaskApi.getClosedTasks();
-        } else if (this.taskState === TaskState.ALL) {
-          response = await TaskApi.getAllTasks();
-        }
-        return response?.data
-      } catch (err) {
-        console.log('error loadQuote: ' + err)
-      }
-    },
-  }
+const props = defineProps({
+  taskState: Object as PropType<TaskState>
 });
+
+let tasks = reactive<TaskRequest[]>([])
+
+const deleteTask = async (id: number) => {
+  if (confirm('Are you sure, you want to delete?')) {
+    const res = await TaskApi.deleteTask(id);
+
+    res.status === 200
+        ? (tasks = tasks.filter((task) => task.id !== id))
+        : alert('Error while deleting task');
+
+    window.location.reload();
+  }
+}
+
+async function fetchTasks() {
+  try {
+    let response = null;
+    if (props.taskState === TaskState.OPEN) {
+      response = await TaskApi.getOpenTasks();
+    } else if (props.taskState === TaskState.CLOSED) {
+      response = await TaskApi.getClosedTasks();
+    } else if (props.taskState === TaskState.ALL) {
+      response = await TaskApi.getAllTasks();
+    }
+    response?.data.forEach((task: TaskRequest) => tasks.push(task));
+  } catch (err) {
+    console.log('error loadQuote: ' + err)
+  }
+}
+
+fetchTasks()
+
 </script>
 
 <style scoped>
