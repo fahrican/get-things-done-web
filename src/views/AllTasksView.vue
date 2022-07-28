920d75276713bf676070df538f05687cfac7b599
fahri <fahri-can@gmx.at>
2022-07-28 19:08:00 +0200

    Use setup in script tag.

--- src/views/AllTasksView.vue
+++ src/views/AllTasksView.vue
@@ -3,23 +3,13 @@
   <Tasks :task-state="taskState"/>
 </template>
 
-<script lang="ts">
-import {defineComponent, ref} from "vue";
+<script lang="ts" setup>
+import {ref} from "vue";
 import Tasks from "@/components/Tasks.vue";
 import {TaskState} from "@/types/TaskState";
 
+const taskState = ref(TaskState.ALL)
 
-export default defineComponent({
-  name: "AllTasksView",
-  components: {
-    Tasks
-  },
-  setup() {
-    const taskState = ref(TaskState.ALL)
-
-    return {taskState}
-  }
-});
 </script>
 
 <style scoped>
