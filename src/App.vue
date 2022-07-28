18f531901a46ea6cda4c424b69ab017480356ac5
fahri <fahri-can@gmx.at>
2022-07-28 18:08:37 +0200

    Move CSS for editing & creating task in central place.

--- src/App.vue
+++ src/App.vue
@@ -54,4 +54,81 @@ h1 {
   position: relative;
   background-color: #f1f1f1;
 }
+
+/* CSS for EditTask & CreateTask */
+
+.form-inline {
+  display: flex;
+  flex-flow: row wrap;
+  flex-direction: column;
+  align-items: stretch;
+  margin: 0.5rem auto;
+}
+
+.form-inline label {
+  margin: 5px auto 5px auto;
+  display: inline-block;
+}
+
+.form-inline input {
+  vertical-align: middle;
+  margin: 5px auto 5px auto;
+  padding: 10px;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  display: inline-block;
+}
+
+.user-input {
+  width: 50%;
+}
+
+.submit-btn {
+  padding: 10px 20px;
+  background-color: dodgerblue;
+  border: 1px solid #ddd;
+  color: white;
+  cursor: pointer;
+  display: flex;
+  margin: 0 auto;
+  width: 20%;
+  text-align: center;
+  justify-content: center;
+}
+
+.priority-label {
+  float: left !important;
+  display: flex !important;
+}
+
+
+.form-inline button:hover {
+  background-color: royalblue;
+}
+
+@media (max-width: 800px) {
+  .form-inline input {
+    margin: 10px auto;
+  }
+
+  .user-input {
+    width: 90%;
+  }
+
+  .submit-btn {
+    padding: 10px 20px;
+    background-color: dodgerblue;
+    border: 1px solid #ddd;
+    color: white;
+    cursor: pointer;
+    max-width: 90%;
+    text-align: center;
+    margin: 0 auto;
+    width: 90%;
+    justify-content: center;
+  }
+}
+
+/* CSS for EditTask & CreateTask */
+
 </style>
